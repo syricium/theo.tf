f34f4824fdfe68f20f9eba1b341c34e52c963956
theo <theo@749.city>
2023-11-10 21:33:52 +0100

    fix weird issues

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/*.html",
+      "./src/static/js/*.js"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
